2d8890fe73a70048763b98460e2539d7d448d038
Andreas <andreas.c.thors1@gmail.com>
2025-10-11 01:41:46 +0200

    added 1/2 of anode effluent valve

---
+++ python/anode_effluent_valve.py
@@ -0,0 +1,25 @@
+from parameters import *
+from objects import Mols
+
+def anode_valve_effluent(anode_tank: 'Tank'):
+
+    anode_pressure = anode_tank.pressure
+    anode_pressure_delta = anode_pressure - ANODE_EXTERNAL_PRESSURE
+    Y = 1 - min(1, (2/3)*(anode_pressure_delta /anode_pressure))
+    gas_volume = anode_tank.gas_volume
+    sum_y_times_Mi = 0
+    for key in ["H2O","O2","H2"]:
+        sum_y_times_Mi += anode_tank.gas_mol.get(key)*anode_tank.gas_mass.get(key)
+    sqrt1 = anode_pressure_delta/( VALVE_SCALING_PRESSURE*VALVE_SCALING_GAS_DENSITY*gas_volume)
+    sqrt2 = 1/(sum_y_times_Mi)
+
+    for key in ["H2O","O2","H2"]:
+
+
+
+    mols = Mols()
+    pass 
+
+
+if __name__ == "__main__":
+    from objects.tank import Tank
