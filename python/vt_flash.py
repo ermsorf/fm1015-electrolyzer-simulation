a6df7c5edf82305d91fe90add0eebdcdc984b3d5
Andreas <andreas.c.thors1@gmail.com>
2025-10-12 19:13:30 +0200

    updated vt flash to use bernt's new formula

--- python/vt_flash.py
+++ python/vt_flash.py
@@ -30,12 +30,12 @@ H_maxh2 = 7.54*10**4*101325 #Hmax
 H_maxo2 = 7.08*10**4*101325
 T_maxh2 = 1/(3.09*10**-3)   #Tmax
 T_maxo2 = 1/(2.73*10**-3)
-T_ch2   = 33.2              #Critical temp
-T_co2   = 154.6
-T_dimh2 = (1/T_maxh2-1/T_ch2)/(1/T-1/T_ch2) #Dimensionless T
-T_dimo2 = (1/T_maxo2-1/T_co2)/(1/T-1/T_co2)
-H_dimh2  = 10**-(1.142-2.846/T_dimh2+2.487/T_dimh2**2-0.9761/T_dimh2**3+0.2001/T_dimh2**4) #Dimension H
-H_dimo2  = 10**-(1.142-2.846/T_dimo2+2.487/T_dimo2**2-0.9761/T_dimo2**3+0.2001/T_dimo2**4)
+T_ch2o  = 641.7             #Critical temp [K]
+
+T_dimh2 = (1/T_maxh2-1/T_ch2o)/(1/T-1/T_ch2o) #Dimensionless T
+T_dimo2 = (1/T_maxo2-1/T_ch2o)/(1/T-1/T_ch2o)
+H_dimh2  = 10**-(1.142-2.846/T_dimh2+2.486/T_dimh2**2-0.9761/T_dimh2**3+0.2001/T_dimh2**4) #Dimensionless H
+H_dimo2  = 10**-(1.142-2.846/T_dimo2+2.486/T_dimo2**2-0.9761/T_dimo2**3+0.2001/T_dimo2**4)
 
 
 H_h2h2o = H_maxh2*H_dimh2 #H value for species
@@ -71,8 +71,6 @@ def vtflash(V,T,n):
     ptot_a = p_a[0] + R*T/V_g*(n_ag[1]+n_ag[2])
     Vspec__a_g = V_g/ntot_ag
     Vspec__a_l = V_l/ntot_al
-    print(p_a)
-    print(n_ag)
     return (x_a, y_a, ntot_al, ntot_ag, Vspec__a_l, Vspec__a_g, ptot_a)
 # "Constants" and other formula expected to be found elsewhere in code
 
