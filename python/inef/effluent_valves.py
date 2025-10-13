a4dc5b414e05d8c4bbabeced702f05b940173bba
ermsorf <erland.sorflaten@gmail.com>
2025-10-13 22:03:40 +0200

    add temporary values

--- python/inef/effluent_valves.py
+++ python/inef/effluent_valves.py
@@ -20,7 +20,8 @@ def general_valve_effluent(tank: 'Tank', tank_type = Literal["anode", "cathode"]
     influent_density = sum_mass / tank.gas_volume # confirm that this is gas
     pressure_sqrt = pressure_delta * influent_density / scaling_factors
     Y = 1 - min(1, (2/3)*(pressure_delta /pressure))
-    # TODO find these values
+    valve_control_signal = 1  # TODO find this value
+    mass_flow_capacity = 0.1  # TODO find these values
     valve_mass_flow = valve_control_signal*mass_flow_capacity*Y*pressure_sqrt
 
     valve_mole_flow = valve_mass_flow/sum_mass
