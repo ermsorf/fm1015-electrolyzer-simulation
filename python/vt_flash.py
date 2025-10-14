d39cd216d08a8b22a5bc8eca488babae757b0b8a
Andreas <andreas.c.thors1@gmail.com>
2025-10-14 15:58:34 +0200

    pulled global calculations into functions

--- python/vt_flash.py
+++ python/vt_flash.py
@@ -8,72 +8,87 @@ mols_gas = np.zeros(3) # Array to hold number of moles in anode, gas
 mols_liquid = np.zeros(3) # Array to hold number of moles in anode, liquid
 pressure = np.zeros(3) # Pressure in anode
 
-
-
 # liquid saturation
-water_saturation_pressure = 10**(ANTOINE_A - ANTOINE_B/(ANTOINE_C+T+KELVIN_TO_CELSIUS))*BAR_TO_PA #Saturation pressure of H2O at T (54) [Pa]
-SATURATION_A = 2.23
-SATURATION_B = -3.332e-3
-SATURATION_C = 6.421e-6
-specific_saturation_volume_H2O = (SATURATION_A+SATURATION_B*T+SATURATION_C*T**2)*1e-5 #Specific volume of H2O at T (55) [m^3/mole]
+def water_saturation_pressure():
+    saturation_pressure = 10**(ANTOINE_A - ANTOINE_B/(ANTOINE_C+T+KELVIN_TO_CELSIUS))*BAR_TO_PA #Saturation pressure of H2O at T (54) [Pa]
+    return saturation_pressure
+
+def water_saturation_volume():
+    SATURATION_A = 2.23
+    SATURATION_B = -3.332e-3
+    SATURATION_C = 6.421e-6
+    specific_saturation_volume_H2O = (SATURATION_A+SATURATION_B*T+SATURATION_C*T**2)*1e-5 #Specific volume of H2O at T (55) [m^3/mole]
+    return specific_saturation_volume_H2O
+
 # Phi
-PHI_CONSTANT = 1.0012
-PHI_SCALING = -1.6e-3
-phi_exponential = np.exp(8.7*((T+KELVIN_TO_CELSIUS)/373.15))
-phi_liquid_saturation_H2O = PHI_CONSTANT + PHI_SCALING*phi_exponential
+def phi():
+    PHI_CONSTANT = 1.0012
+    PHI_SCALING = -1.6e-3
+    phi_exponential = np.exp(8.7*((T+KELVIN_TO_CELSIUS)/373.15))
+    phi_liquid_saturation_H2O = PHI_CONSTANT + PHI_SCALING*phi_exponential
+    return phi_liquid_saturation_H2O
+
 #Fugacity
-fugacity_liquid_saturation_H2O = phi_liquid_saturation_H2O*water_saturation_pressure 
+def fugacity():
+    f = phi()*water_saturation_pressure()
+    return f
 
-Henry_max_H2 = 7.54e4*ATM_TO_PA #Hmax
-Henry_max_O2 = 7.08e4*ATM_TO_PA
-Max_Temperature_H2 = 1/(3.09e-3)   #Tmax
-Max_Temoperature_O2 = 1/(2.73e-3) 
-Critical_temperature_H2O  = 641.7 #Critical temp [K]
+def Henry(element):
+    assert element in ["H2", "O2"]
+    Henry_max_H2 = 7.54e4*ATM_TO_PA #Hmax
+    Henry_max_O2 = 7.08e4*ATM_TO_PA
+    Max_Temperature_H2 = 1/(3.09e-3)   #Tmax
+    Max_Temoperature_O2 = 1/(2.73e-3) 
+    Critical_temperature_H2O  = 641.7 #Critical temp [K]
 
-Temperature_H2 = (1/Max_Temperature_H2-1/Critical_temperature_H2O)/(1/T-1/Critical_temperature_H2O) #Dimensionless T
-Temperature_O2 = (1/Max_Temoperature_O2-1/Critical_temperature_H2O)/(1/T-1/Critical_temperature_H2O)
-# Shorthands for next formula
-TH2 = Temperature_H2 
-TO2 = Temperature_O2
-Henry_H2  = 10**-(1.142-2.846/TH2+2.486/TH2**2-0.9761/TH2**3+0.2001/TH2**4) #Dimensionless H
-Henry_O2  = 10**-(1.142-2.846/TO2+2.486/TO2**2-0.9761/TO2**3+0.2001/TO2**4)
+    Temperature_H2 = (1/Max_Temperature_H2-1/Critical_temperature_H2O)/(1/T-1/Critical_temperature_H2O) #Dimensionless T
+    Temperature_O2 = (1/Max_Temoperature_O2-1/Critical_temperature_H2O)/(1/T-1/Critical_temperature_H2O)
+    # Shorthands for next formula
+    TH2 = Temperature_H2 
+    TO2 = Temperature_O2
+    Henry_H2  = 10**-(1.142-2.846/TH2+2.486/TH2**2-0.9761/TH2**3+0.2001/TH2**4) #Dimensionless H
+    Henry_O2  = 10**-(1.142-2.846/TO2+2.486/TO2**2-0.9761/TO2**3+0.2001/TO2**4)
 
-Henry_Fraction_H2 = Henry_max_H2*Henry_H2 #H value for species
-Henry_Fraction_O2 = Henry_max_O2*Henry_O2
+    Henry_Fraction_H2 = Henry_max_H2*Henry_H2 #H value for species
+    Henry_Fraction_O2 = Henry_max_O2*Henry_O2
+    if element == "H2":
+        return Henry_Fraction_H2
+    elif element == "O2":
+        return Henry_Fraction_O2
 
 
 def vtflash(V,T,n):
-    pressure[0] = water_saturation_pressure #Ph20 = psath2o, eq 34
+    pressure[0] = water_saturation_pressure() #Ph20 = psath2o, eq 34
     
     R = IDEAL_GAS_CONSTANT # J/mol*K
     # TODO Split up into readable chunks:
-    mols_liquid[0] = (fugacity_liquid_saturation_H2O*V-n[0]*T*R)/(fugacity_liquid_saturation_H2O*specific_saturation_volume_H2O-R*T)
+    mols_liquid[0] = (fugacity()*V-n[0]*T*R)/(fugacity()*water_saturation_volume()-R*T)
     mols_gas[0] = n[0]-mols_liquid[0]
     mols_gas[1] = n[1]
     mols_gas[2] = n[2]
 
-    Volume_liquid = mols_liquid[0]*specific_saturation_volume_H2O
+    Volume_liquid = mols_liquid[0]*water_saturation_volume()
     Volume_gas = V - Volume_liquid
     
     pressure[1] = mols_gas[1]*R*T/Volume_gas
     pressure[2] = mols_gas[2]*R*T/Volume_gas
     total_pressure = sum(pressure)
     
-    gas_count = sum(mols_gas)
-    gas_fraction_y =mols_gas/gas_count
-    mols_liquid[1] = mols_liquid[0]*(pressure[1]/Henry_Fraction_H2)
-    mols_liquid[2] = mols_liquid[0]*(pressure[2]/Henry_Fraction_O2)
-    liquid_count = sum(mols_liquid)
-    liquid_fraction_x =mols_liquid/liquid_count
+    total_moles_gas = sum(mols_gas)
+    gas_fraction_y =mols_gas/total_moles_gas
+    mols_liquid[1] = mols_liquid[0]*(pressure[1]/Henry("H2"))
+    mols_liquid[2] = mols_liquid[0]*(pressure[2]/Henry("O2"))
+    total_moles_liquid = sum(mols_liquid)
+    liquid_fraction_x =mols_liquid/total_moles_liquid
     
     mols_gas[1] -= mols_liquid[1]    
     mols_gas[2] -= mols_liquid[2]
-    gas_count = sum(mols_gas)
-    gas_fraction_y = mols_gas/gas_count
+    total_moles_gas = sum(mols_gas)
+    gas_fraction_y = mols_gas/total_moles_gas
     total_pressure = pressure[0] + R*T/Volume_gas*(mols_gas[1]+mols_gas[2])
-    specific_Volume_gas = Volume_gas/gas_count
-    specific_Volume_liquid = Volume_liquid/liquid_count
-    return (liquid_fraction_x, gas_fraction_y, liquid_count, gas_count, specific_Volume_liquid, specific_Volume_gas, total_pressure)
+    specific_Volume_gas = Volume_gas/total_moles_gas
+    specific_Volume_liquid = Volume_liquid/total_moles_liquid
+    return (liquid_fraction_x, gas_fraction_y, total_moles_liquid, total_moles_gas, specific_Volume_liquid, specific_Volume_gas, total_pressure)
 # "Constants" and other formula expected to be found elsewhere in code
 
 
